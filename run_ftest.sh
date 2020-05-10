8fb459e6f82feeea6853b45ddf41efe93e11f5e2
Nelson Silva <nsilva@nuxeo.com>
2020-05-10 16:58:11 +0100

    Update test url

--- run_ftest.sh
+++ run_ftest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "@nuxeo:registry=https://packages.nuxeo.com/repository/npm-public" >> ~/.npmrc
-npx @nuxeo/nuxeo-web-ui-ftest@3.0.0-SNAPSHOT --report --screenshots --headless --tags='not @ignore'
+npx @nuxeo/nuxeo-web-ui-ftest@3.0.0-SNAPSHOT --url=http://localhost/nuxeo --report --screenshots --headless --tags='not @ignore'
