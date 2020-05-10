62b6f3b896a884949b66b2e6da7a943f24aa14cf
Nelson Silva <nsilva@nuxeo.com>
2020-05-10 16:25:16 +0100

    Npm registry

--- run_ftest.sh
+++ run_ftest.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo "@nuxeo:registry=https://packages.nuxeo.com/repository/npm-public" >> ~/.npmrc
 npx @nuxeo/nuxeo-web-ui-ftest@3.0.0-SNAPSHOT --report --screenshots --headless --tags='not @ignore'
